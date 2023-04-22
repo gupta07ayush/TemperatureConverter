0ec3a9ac45a1fc46d64900191576c59f6746a0b0
Ayush Gupta <gupta07ayush@gmail.com>
2023-04-22 18:48:45 +0530

    hide unnessesory colors name data

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from tkinter import *
 
+# importing Mycolors class from mycolors module
+from mycolors import Mycolor
+
+# making object of Mycolor Class so that we can access all the variables and methods of Mycolor
+obj = Mycolor()
+
 root = Tk()
 root.geometry("1200x500")
 root.title("Temperature Convertor")
 root.config(bg='#ADE4DB')
 
 
-coolcolors = ['#000000', '#00001a', '#000033', '#00004d', '#000066', '#000080', '#000099', '#0000b3', '#0000cc', '#0000e6', '#0000ff', '#1a1aff', '#3333ff', '#4d4dff', '#6666ff', '#8080ff', '#9999ff', '#b3b3ff', '#ccccff','#c6d9ec']
-warmcolors = ['ffc2b3', '#ffcccc','#ffb3b3','#ff9999','#ff8080','#ff6666','#ff4d4d','#ff3333','#ff1a1a', '#ff0000','#e60000','#cc0000','#b30000','#990000','#800000','#660000','#4d0000','#330000','#1a0000','#000000']
-colorlist = coolcolors + warmcolors
-
-colordict = {round(35.0 + i/10, 1): colorlist[i] for i in range(len(colorlist))}
+colordict = {round(35.0 + i/10, 1): obj.colorlist[i] for i in range(len(obj.colorlist))}
 
 
 def myplus():
@@ -62,8 +64,6 @@ plus.place(x=460, y=210, height=80, width=100)
 
 minus = Button(root, text="-", bg="#9A031E", fg="#8ecae6", font=("verdana", 50, 'bold'), command=myminus)
 minus.place(x=600, y=210, height=80, width=100)
-
-
 
 
 
